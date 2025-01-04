139bc7fb1e1d6584a8e3e2c81fa1d11e794baef9
makoto-developer <72484465+makoto-developer@users.noreply.github.com>
2025-01-04 10:32:23 +0900

    add param check

--- sort/basic_sort.js
+++ sort/basic_sort.js
@@ -1,14 +1,21 @@
-const a = [80, 50, 56, 30, 51, 70];
-const N = a.length; // データ数
-for (let i = 0; i < N - 1; i++) {
-  let mins = a[i];
-  let s = i;
-  for (let j = i + 1; j < N; j++) {
-    if (a[j] < mins) {
-      mins = a[j];
-      s = j;
+/*
+ * a -> list
+ * example -> a:[80, 50, 56, 30, 51, 70]
+ */
+function basic_sort(a) {
+  if (!Array.isArray(a)) throw new Error("Parameter is not a list");
+  const N = a.length;
+  if (N < 1) throw new Error("Parameter is empty list");
+  for (let i = 0; i < N; i++) {
+    let mins = a[i];
+    let s = i;
+    for (let j = i + 1; j < N; j++) {
+      if (a[j] < mins) {
+        mins = a[j];
+        s = j;
+      }
     }
   }
+  [a[i], a[s]] = [a[s], a[i]];
+  return a;
 }
-[a[i], a[s]] = [a[s], a[i]]; // a[i]とa[s]の交換
-console.log(a);
